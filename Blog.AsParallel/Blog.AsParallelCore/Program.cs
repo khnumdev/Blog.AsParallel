61bfffc4c24ce1340f7a49fd964e003ec5a95abc
aperez <isisosirishorus@hotmail.com>
2016-03-26 22:07:55 +0100

    Added DNX project.

--- Blog.AsParallel/Blog.AsParallelCore/Program.cs
+++ Blog.AsParallel/Blog.AsParallelCore/Program.cs
@@ -1,13 +1,12 @@
-﻿namespace Blog.AsParallel
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
-    class Program
+namespace Blog.AsParallelCore
+{
+    public class Program
     {
         static void Main(string[] args)
         {
---
+++ Blog.AsParallel/Blog.AsParallelCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Blog.AsParallelCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blog.AsParallelCore")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a4be46b7-f756-406b-81db-ed9d9807b598")]
